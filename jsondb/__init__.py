bc68e131f9fa7fa41316c4c6b0f7a03ebd6a3397
Gunther Cox <gunthercx@gmail.com>
2016-12-29 13:35:17 -0500

    Update version to 0.1.7

--- jsondb/__init__.py
+++ jsondb/__init__.py
@@ -4,7 +4,7 @@ A flat file database for json objects.
 
 from .db import Database
 
-__version__ = '0.1.6'
+__version__ = '0.1.7'
 __author__ = 'Gunther Cox'
 __email__ = 'gunthercx@gmail.com'
 __url__ = 'https://github.com/gunthercox/jsondb'
